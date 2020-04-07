73d7a406e951fd24da742e4814f76f7ccc937b7c
julianajjo <juliana_jesusoliveira@yahoo.com.br>
2020-04-06 23:42:09 -0300

    Cadastrar um fabricante

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,5 +1,21 @@
 class ManufacturersController < ApplicationController
     def index
-      @manufacturers = Manufacturer.all     
+      @manufacturers = Manufacturer.all
     end
+
+    def show
+      @manufacturer = Manufacturer.find(params[:id])
+    end
+
+    def new
+      @manufacturer = Manufacturer.new
+    end
+
+    def create
+      @manufacturer = Manufacturer.new
+      @manufacturer.name = params[:manufcturer][:name]
+      @manufacturer.save
+      redirect_to @manufacturer
+    end
+
 end

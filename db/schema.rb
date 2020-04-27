dbb7ee78e9cf891d5e2e624e97a9647bcc01b66a
julianajjo <juliana_jesusoliveira@yahoo.com.br>
2020-04-27 16:22:57 -0300

    Adicionando login: usuário e senha com a gem devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_200921) do
+ActiveRecord::Schema.define(version: 2020_04_27_184615) do
 
   create_table "car_categories", force: :cascade do |t|
     t.string "name"
@@ -57,6 +57,18 @@ ActiveRecord::Schema.define(version: 2020_04_22_200921) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["car_category_id"], name: "index_rentals_on_car_category_id"
     t.index ["client_id"], name: "index_rentals_on_client_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "car_models", "car_categories"

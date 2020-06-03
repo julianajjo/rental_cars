480bce10c474e550f4ce16f85d008a9ac96af7e5
julianajjo <juliana_jesusoliveira@yahoo.com.br>
2020-06-02 23:07:58 -0300

    Nested resources: car_rental

--- app/controllers/car_rentals_controller.rb
+++ app/controllers/car_rentals_controller.rb
@@ -1,11 +1,12 @@
 class CarRentalsController < ApplicationController
   def create
     @car_rental = CarRental.new(car_rentals_params)
-    @rental = @car_rental.rental
+    @rental = Rental.find(params[:rental_id])
 
     @car_rental.user = current_user
     @car_rental.start_date = Time.zone.now
 
+    @car_rental.rental = @rental
     @rental.ongoing!
     @car_rental.car.rented!
     @car_rental.daily_rate = @rental.car_category.daily_rate
@@ -17,7 +18,7 @@ class CarRentalsController < ApplicationController
   private
 
   def car_rentals_params
-    params.require(:car_rental).permit(:car_id, :rental_id, add_on_ids: [])
+    params.require(:car_rental).permit(:car_id, add_on_ids: [])
   end
  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
   resources :car_categories, only: %i[index new create show]
   resources :car_models, only: %i[index new create show]
   resources :rentals, only: %i[index new create show] do
+    resources :car_rentals, only: [:create]
     get 'search', on: :collection
     get 'start', on: :member
     # post 'start', on: :member, to: 'rentals#confirm' -> trocamos pelo CarRentals#create
   end
-  resources :car_rentals, only: [:create]
 end
+ 
